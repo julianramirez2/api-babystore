6371eaf1f12cdcd03d2934fe13b02a1b8815e2de
Liz Rodrigues <liz.oriana14@gmail.com>
2021-11-13 19:52:51 -0500

    first commit

---
+++ app/Models/Product.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+
+class Product extends Model
+{
+    use HasFactory;
+    public $timestamps = false;
+
+    protected $fillable = ['name','description','price','category','stock','image'];
+
+    public function getId()
+    {
+        return $this->attributes['id'];
+    }
+
+    public function setId($id)
+    {
+        $this->attributes['id'] = $id;
+    }
+
+    public function getName()
+    {
+        return $this->attributes['name'];
+    }
+
+    public function setName($name)
+    {
+        $this->attributes['name'] = $name;
+    }
+
+    public function getDesc()
+    {
+        return $this->attributes['description'];
+    }
+
+    public function setDesc($description)
+    {
+        $this->attributes['description'] = $description;
+    }
+
+    public function getPrice()
+    {
+        return $this->attributes['price'];
+    }
+
+    public function setPrice($price)
+    {
+        $this->attributes['price'] = $price;
+    }
+
+    public function getCat()
+    {
+        return $this->attributes['category'];
+    }
+
+    public function setCat($category)
+    {
+        $this->attributes['category'] = $category;
+    }
+
+    public function getStock()
+    {
+        return $this->attributes['stock'];
+    }
+
+    public function setStock($stock)
+    {
+        $this->attributes['stock'] = $stock;
+    }
+
+    public function getImage()
+    {
+        return $this->attributes['image'];
+    }
+
+    public function setImage($image)
+    {
+        $this->attributes['image'] = $image;
+    }
+
+    public function items()
+    {
+        return $this->hasMany(Item::class);
+    }
+}
